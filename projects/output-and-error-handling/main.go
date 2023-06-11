d5aab4a52caaffffd17336a5ecde0630763b0559
Francine Blanc <francine.blanc@gmail.com>
2023-06-11 21:20:08 +0100

    add additional outcomes for 429 status code

--- projects/output-and-error-handling/main.go
+++ projects/output-and-error-handling/main.go
@@ -28,9 +28,23 @@ func client() {
 	if resp.StatusCode == 200 {
 		fmt.Println(string(body))
 	} else if resp.StatusCode == 429 {
-		if secs, _ := strconv.Atoi(resp.Header.Get("Retry-After")); secs >= 1 && secs <= 10 {
+		if resp.Header.Get("Retry-After") == "a while" {
+			fmt.Println("Experiencing high amounts of traffic...retrying again...")
+			time.Sleep(time.Duration(4) * time.Second)
+			client()
+		} else if secs, _ := strconv.Atoi(resp.Header.Get("Retry-After")); secs >= 1 && secs < 5 {
 			fmt.Println("Retrying in", secs, "seconds...")
 			time.Sleep(time.Duration(secs) * time.Second)
+			client()
+		} else if secs, _ := strconv.Atoi(resp.Header.Get("Retry-After")); secs >= 5 {
+			fmt.Println("Weather cannot be retrieved :(")
+		} else {
+			t, _ := http.ParseTime(resp.Header.Get("Retry-After"))
+			unixTime := t.UTC().Unix()
+			currentTime := time.Now().UTC().Unix()
+			retrySecs := unixTime - currentTime
+			fmt.Println("Retrying in", retrySecs, "seconds...")
+			time.Sleep(time.Duration(retrySecs) * time.Second)
 			client()
 		}
 	}
