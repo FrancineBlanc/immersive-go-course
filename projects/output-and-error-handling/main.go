c9a8885d4cc23cede6e3d55510ebc62fcc9be559
Francine Blanc <francine.blanc@gmail.com>
2023-06-12 16:17:45 +0100

    create new func for success, create error log

--- projects/output-and-error-handling/main.go
+++ projects/output-and-error-handling/main.go
@@ -10,31 +10,38 @@ import (
 )
 
 func main() {
-	_, err, _, _ := getRequest()
+	body, _, statusCode, err := getRequest()
 	if err != nil {
+		os.Stderr, _ = os.Create("stderr-log.txt")
 		fmt.Println(err)
+		fmt.Fprintf(os.Stderr, "Connection dropped: %v\n", err)
 		os.Exit(1)
-	} else {
-		client()
+	} else if statusCode == 200 {
+		successCall(body)
 	}
+	// TO CHECK - What if there was more code in main, and an err has occurred?
 }
 
-func getRequest() (string, error, int, string) {
+func getRequest() (string, string, int, error) {
 	resp, err := http.Get("http://localhost:8080")
 	if err != nil {
-		return "", fmt.Errorf("the weather cannot be retrieved right now"), 0, ""
+		return "", "", 0, fmt.Errorf("the weather cannot be retrieved right now")
 	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return "", fmt.Errorf("unable to read weather data at present"), 0, ""
+		return "", "", 0, fmt.Errorf("unable to read weather data at present")
 	}
-	return string(body), nil, resp.StatusCode, resp.Header.Get("Retry-After")
+	return string(body), resp.Header.Get("Retry-After"), resp.StatusCode, nil
+}
+
+func successCall(respBody string) {
+	fmt.Println(respBody)
 }
 
 func client() {
-	body, _, statusCode, retryAfter := getRequest()
+	body, retryAfter, statusCode, _ := getRequest()
 	if statusCode == 200 {
 		fmt.Println(string(body))
 	} else if statusCode == 429 {
@@ -53,10 +60,13 @@ func client() {
 			unixTime := t.UTC().Unix()
 			currentTime := time.Now().UTC().Unix()
 			retrySecs := unixTime - currentTime
-			fmt.Println("Retrying in", retrySecs, "seconds...")
-			time.Sleep(time.Duration(retrySecs) * time.Second)
-			client()
+			if retrySecs > 5 {
+				fmt.Println("Weather cannot be retrieved :(")
+			} else {
+				fmt.Println("Retrying in", retrySecs, "seconds...")
+				time.Sleep(time.Duration(retrySecs) * time.Second)
+				client()
+			}
 		}
 	}
-
 }
--- projects/output-and-error-handling/server/main.go
+++ projects/output-and-error-handling/server/main.go
@@ -12,8 +12,8 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		// We generate a random number between 0 and 9 (inclusive), so that we can decide whether to behave properly (half of the time), or simulate error conditions.
-		randomNumber := rand.Intn(10)
-		// randomNumber := 8
+		// randomNumber := rand.Intn(10)
+		randomNumber := 1
 		switch randomNumber {
 		// 50% of the time, we just report the weather. 30% nice, 20% less so.
 		case 0, 1, 2:
